3f7b339a1cfec7ef2cb8f39c9ce10b00269786c1
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2023-06-08 00:47:56 +0300

    Initial Commit

---
+++ UICollectionView_Albums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView_Albums
+//
+//  Created by D K on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
